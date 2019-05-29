3825c1ad242def6c8a3122006fee3cb06a77b486
Thomas Wilburn <twilburn@npr.org>
2019-05-29 09:41:31 -0500

    Add currentWindow filter to the query

--- background.js
+++ background.js
@@ -2,10 +2,10 @@ browser.commands.onCommand.addListener(async function(cmd) {
   var [ _, index ] = cmd.match(/tab-(.)/);
   var tab;
   if (index == "9") {
-    tab = (await browser.tabs.query({})).pop();
+    tab = (await browser.tabs.query({ currentWindow: true })).pop();
   } else {
     index -= 1;
-    [ tab ] = await browser.tabs.query({ index });
+    [ tab ] = await browser.tabs.query({ currentWindow: true, index });
   }
   await browser.tabs.update(tab.id, { active: true });
 });
---
+++ readme.rst
@@ -0,0 +1,4 @@
+Ctrl-Digit
+==========
+
+It's not complicated: allows Mac and Linux versions of Firefox to use Ctrl+1-9 to switch tabs like Windows does, so that I don't hammer at the wrong shortcut like a dopamine-addled lab rat when I hop platforms.
